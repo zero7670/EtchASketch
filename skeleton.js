6fa8775861c56497f08620280cae2cddd4a74c92
Khang Dao <zero7679@gmail.com>
2023-07-03 14:59:30 -0400

    Etch A Sketch design and functionality completed

---
+++ skeleton.js
@@ -0,0 +1,38 @@
+
+function myFunction(){
+    
+    
+    const container = document.querySelector('#container');
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+      }
+    let sign = prompt("Choose your grid size (0-100)");
+
+    if (sign > 0 && sign < 100){
+        for (let i = 0; i < sign; i++){
+            const smallContainer = document.createElement('div');
+            smallContainer.classList.add('smallContainer')
+            for (let j = 0; j < sign; j++){  
+                const contentTwo = document.createElement('div');
+                contentTwo.classList.add('contentTwo');
+                smallContainer.appendChild(contentTwo); 
+                            
+            }
+            container.appendChild(smallContainer);
+        }
+    }
+    else
+        alert("ERROR, OUT OF RANGE")
+    container.addEventListener("mouseover", event => {
+    event.target.style.backgroundColor = "black";
+    })
+    
+}
+
+
+
+
+
+
+
+
